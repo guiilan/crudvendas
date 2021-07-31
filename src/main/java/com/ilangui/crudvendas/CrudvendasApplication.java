bc9a03995b1847db3aef77169ccfe8dfae9faa7f
guiilan <gui.tioze@gmail.com>
2021-07-30 23:36:51 -0300

    iniciando

---
+++ src/main/java/com/ilangui/crudvendas/CrudvendasApplication.java
@@ -0,0 +1,13 @@
+package com.ilangui.crudvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilangui/crudvendas/CrudvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilangui.crudvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
